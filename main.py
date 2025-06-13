dbd9a7d05d012e76c6ad46e491778e89d5e3a152
yuvasakthi S <155764035+Sakthiyuva@users.noreply.github.com>
2025-06-13 11:05:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello yuvasakthi, this is new enrty", 200
+    return "Hello yuvasakthi", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
